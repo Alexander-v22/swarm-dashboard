cbd89f8b7cebcd5be17796ba385be983445de0d7
av275 <av275@duke.edu>
2025-09-17 10:54:13 -0400

    Setting up live stream from pi5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { PI_URL } from '$lib/../secrets.js'; // adjust path if needed
+</script>
+
+<main class="flex-1 justify-center">
+  <img src={PI_URL} alt="YOLO live stream" style="max-width:100%;" />
+</main>
+
